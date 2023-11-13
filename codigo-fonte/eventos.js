e514f6a952c9407b41d367effb4fc3f837ae69d6
Ana Laura F. B. Reis <analaurafbr@gmail.com>
2023-11-13 15:20:23 -0300

    Atualizações

---
+++ codigo-fonte/eventos.js
@@ -0,0 +1,39 @@
+[
+    {
+        id: 0,
+        event_name: "Festival da Farinha Podre",
+        date: "02/12/2023",
+        genre: "rock",
+        thumbnail_adress: ""
+    },
+    {
+        id: 1,
+        event_name: "Festival da Farinha Podre",
+        date: "02/12/2023",
+        genre: "rock"
+    },
+    {
+        id: 2,
+        event_name: "Festival da Farinha Podre",
+        date: "02/12/2023",
+        genre: "rock"
+    },
+    {
+        id: 3,
+        event_name: "Festival da Farinha Podre",
+        date: "02/12/2023",
+        genre: "rock"
+    },
+    {
+        id: 4,
+        event_name: "Festival da Farinha Podre",
+        date: "02/12/2023",
+        genre: "rock"
+    },
+    {
+        id: 5,
+        event_name: "Festival da Farinha Podre",
+        date: "02/12/2023",
+        genre: "rock"
+    }
+]
---
+++ codigo-fonte/pagina_sobre/sobre.js
@@ -0,0 +1,25 @@
+import eventos from eventos.js
+
+function filtrar(filtro, eventos) {
+    const filter = filtro;
+    const events = eventos;
+
+    const array_size = size(eventos);
+
+    var result = [array_size]
+
+    for(var i = 0; i < array_size; i++) {
+        const keys = eventos[i].value
+
+    }
+
+    var result = events.array.forEach(element => {
+        
+    });
+
+
+}
+
+function mostrar_filtros() {
+
+}

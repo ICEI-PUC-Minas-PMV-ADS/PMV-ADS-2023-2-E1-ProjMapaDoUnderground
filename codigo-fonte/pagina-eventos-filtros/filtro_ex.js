bc1998a2faa6828801ef237f8f3924f821890347
Ana Laura F. B. Reis <analaurafbr@gmail.com>
2023-11-24 15:23:43 -0300

    Update Filter

--- codigo-fonte/pagina-eventos-filtros/filtro_ex.js
+++ codigo-fonte/pagina-eventos-filtros/filtro_ex.js
@@ -1,3 +1,6 @@
+function showCalendar() {
+    $("#datepicker").datepicker("show");
+}
 function filterElements(category) {
     const elements = document.querySelectorAll(".box");
 
@@ -9,6 +12,7 @@ function filterElements(category) {
         }
     });
 
+    // Aqui você pode adicionar lógica para exibir as caixas ao clicar nos botões do filtro
     const especialBoxG = document.querySelector('.box.Gênero.especial');
     const especialBoxL = document.querySelector('.box.Localização.especial');
     const especialBoxD = document.querySelector('.box.Data.especial');
@@ -26,8 +30,33 @@ function filterElements(category) {
         especialBoxL.classList.add('hide');
         especialBoxD.classList.remove('hide');
     } else {
+        // Se nenhum botão específico for clicado, pode-se ocultar todas as caixas especiais
         especialBoxG.classList.add('hide');
         especialBoxL.classList.add('hide');
         especialBoxD.classList.add('hide');
     }
 }
+
+// Funções para exibir os modais ao clicar nos botões
+function showGenre() {
+    var modal = document.getElementById("genreModal");
+    modal.style.display = "block";
+}
+
+function showLocal() {
+    var modal = document.getElementById("localModal");
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    var modals = document.querySelectorAll(".modal");
+    modals.forEach((modal) => {
+        modal.style.display = "none";
+    });
+}
+$(function() {
+    $("#datepicker").datepicker({
+        dateFormat: "dd/mm/yy" // Formato da data
+        // Outras opções e configurações podem ser adicionadas aqui
+    });
+});

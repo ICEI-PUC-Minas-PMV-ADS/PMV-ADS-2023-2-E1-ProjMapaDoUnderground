ab16fa0fcd7a6e66cecaa9ebaa53945b0ecb4b26
Sara Guanais de Miranda <guanaissara@gmail.com>
2023-11-25 19:02:06 -0300

    Adicionando evento

---
+++ pagina_evento_selecionado_baianasystem/favoritar.js
@@ -0,0 +1,19 @@
+let clicado = false;
+
+document.getElementById('botao').addEventListener('click', function(event) {
+    event.preventDefault(); // Evita o comportamento padrão do link
+
+    if (clicado) {
+        document.getElementById('imagem-normal').style.display = 'block';
+        document.getElementById('imagem-clicada').style.display = 'none';
+    } else {
+        document.getElementById('imagem-normal').style.display = 'none';
+        document.getElementById('imagem-clicada').style.display = 'block';
+    }
+
+    clicado = !clicado;
+});
+
+
+
+

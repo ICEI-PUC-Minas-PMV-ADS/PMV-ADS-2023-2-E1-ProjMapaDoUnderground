f272c5b54dc42771a7d9780379db9654a9f7b511
Ana Laura F. B. Reis <analaurafbr@gmail.com>
2023-11-30 19:27:58 -0300

    Co-authored-by: Thabata Dias de Freitas <thabata02@users.noreply.github.com> Co-authored-by: Larissa Pocceschy <laripocceschy@users.noreply.github.com> Co-authored-by: Sara Guanais de Miranda <SaraGuanais@users.noreply.github.com> Co-authored-by: Mia Nascimento <ohnomavi@users.noreply.github.com>

--- codigo-fonte/pagina_login/menu.js
+++ codigo-fonte/pagina_login/menu.js
@@ -11,3 +11,21 @@ function clickMenu() {
       navLinks.classList.add('show');
     }
   }
+
+  document.getElementById('show-login1').addEventListener('click', function() {
+    document.getElementById('login-usuario').classList.add('active');
+    document.getElementById('login-produtor').classList.remove('active');
+  });
+  
+  document.getElementById('show-login2').addEventListener('click', function() {
+    document.getElementById('login-produtor').classList.add('active');
+    document.getElementById('login-usuario').classList.remove('active');
+  });
+  
+  document.querySelector("#login-usuario .close-btn").addEventListener("click", function() {
+    document.getElementById('login-usuario').classList.remove("active");
+  });
+  
+  document.querySelector("#login-produtor .close-btn").addEventListener("click", function() {
+    document.getElementById('login-produtor').classList.remove("active");
+  });
